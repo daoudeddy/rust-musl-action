4882d95d40420c90e55998b8e8bb036a22c35f01
Eddy Daoud <daoudeddy@icloud.com>
2023-01-21 15:19:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,7 @@ cd $GITHUB_WORKSPACE
 rustup default stable
 rustup target add x86_64-unknown-linux-musl
 rustup target add aarch64-unknown-linux-musl
+rustup target add i686-unknown-linux-musl
+rustup target add armv7-unknown-linux-musleabihf
+
 bash -c "$*"
