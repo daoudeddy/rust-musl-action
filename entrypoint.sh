eb1c7b9a51c62820c46d6a5ada64bb8750765827
Eddy Daoud <daoudeddy@icloud.com>
2023-01-22 09:14:29 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -Eeuxo pipefail
 cd $GITHUB_WORKSPACE
-rustup default stable
-rustup target add x86_64-unknown-linux-musl
-rustup target add aarch64-unknown-linux-musl
-rustup target add i686-unknown-linux-musl
-rustup target add armv7-unknown-linux-musleabihf
-
 bash -c "$*"
